8b25864da24bfa3256c32e10c933f653a7eeacb4
datapluskz <datapluskz@gmail.com>
2021-06-28 15:53:16 +0600

    Upload Discovery Travell App Home Page

---
+++ TravelApp/Application/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Tanirbergen Kaldibai on 13.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
