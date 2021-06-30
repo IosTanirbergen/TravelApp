e0b4b0477c5d1b48201099636405d4d73de8322c
datapluskz <datapluskz@gmail.com>
2021-06-30 17:45:09 +0600

    Updated Data Categories

--- TravelApp/Module/HomeView/HomeData.swift
+++ TravelApp/Module/HomeView/HomeData.swift
@@ -10,3 +10,16 @@ import Foundation
 struct CategoriesItem: Hashable {
     let name, imageName: String
 }
+
+
+struct Destination: Hashable {
+    let imageName, countryName, cityName: String
+}
+
+struct SubCategoriesItem: Hashable {
+    let categoriesTitle, image, rating, venue, price, county, city: String
+}
+
+struct GitsItem: Hashable {
+    let gitName, gitImage: String
+}
